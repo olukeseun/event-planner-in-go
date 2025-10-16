cdfe202ad780c98cb34e83b464b8c31353956197
Olukeye <seun.olukeye@gkpele.org>
2025-10-16 19:18:16 +0100

    feat: removed unused code

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -9,17 +9,17 @@ import (
 )
 
 func main() {
-	// 1. Define the database connection string and path to migration files
+	// Define the database connection string and path to migration files
 	dbURL := "postgres://postgres:shongokeye@localhost:5432/eventplannerDB?sslmode=disable"
 	migrationsPath := "file://cmd/migrate/migrations" // Note the 'file://' prefix
 
-	// 2. Create a new migrate instance
+	// Create a new migrate instance
 	m, err := migrate.New(migrationsPath, dbURL)
 	if err != nil {
 		log.Fatalf("could not create migrate instance: %v", err)
 	}
 
-	// 3. Apply all pending 'up' migrations
+	// Apply all pending 'up' migrations
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		log.Fatalf("migration failed: %v", err)
 	}
