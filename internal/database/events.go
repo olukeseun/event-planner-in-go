b34479f7450c4d437d06d4bd51f479e0fe63b2f7
Seun Olukeye <seun.olukeye@gkpele.org>
2025-10-19 22:28:41 +0100

    Update events.go

--- internal/database/events.go
+++ internal/database/events.go
@@ -1,20 +1,98 @@
-package database
+package main
 
 import (
-	"database/sql"
-	"time"
+	"event-planner-in-go/internal/database"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-type EventModelModel struct {
-	db *sql.DB
+func (app *application) createEvent(c *gin.Context) {
+	var event database.Event
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	err := app.models.Events.Insert(&event)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, event)
 }
 
-// User represents a single user entity in the system, typically mapped to a database table.
-type Event struct {
-	Id          int64     `json:"id" db:"id"`
-	OwnerId     int       `json:"ownerId" db:"ownerId" binding:"required"`
-	Name        string    `json:"name" db:"name" binding:"required, min=3"`
-	Description string    `json:"description" db:"description" binding:"required, min=10"`
-	Date        time.Time `json:"date" db:"date" binding:"required"`
-	Location    bool      `json:"location" db:"location" binding:"required"`
+func (app *application) getEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalide event Id"})
+	}
+
+	event, err := app.models.Events.Get(id)
+
+	if event == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+	}
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+	}
+}
+
+func (app *application) getEvents(c *gin.Context) {
+	events, err := app.models.Events.GetAll()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrive events"})
+	}
+	c.JSON(http.StatusOK, events)
+}
+
+func (app *application) updateEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalide event Id"})
+	}
+
+	existingEvent, err := app.models.Events.Get(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+	}
+
+	if existingEvent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+	}
+
+	updatedEvent := &database.Event{}
+
+	if err := c.ShouldBindJSON(updatedEvent); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.Id = id
+
+	if err := app.models.Events.Update(updatedEvent); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "Failed to uodate event"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedEvent)
+}
+
+func (app *application) deleteEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "Failed to delete event"})
+	}
+
+	if err := app.models.Events.Delete(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "Failed to delete event"})
+		return
+	}
+	c.JSON(http.StatusOK, nil)
 }
